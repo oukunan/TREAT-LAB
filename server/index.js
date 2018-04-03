0bef752f27cdcbe07ecb7a0c09ca53eca8fbbf28
Kunanan <kunanan.tas@mail.kmutt.ac.th>
2018-04-03 21:53:28 +0700

    Prepare for Mameow

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const MongoClient = require("mongodb").MongoClient;
+// const url = ""; 
 
 io.on("connection", function(socket) {
-  console.log("user connected");
+  console.log("User connected");
 
   socket.on("test", function(data) {
     socket.emit("render", data);
   });
+
+  // MongoClient.connect(url, function(err, db) {
+  //   if (err) throw err;
+  //   let dbo = db.db("helloDB");
+  //   dbo.collection("duumy").findOne({}, function(err, data) {
+  //     if (err) throw err;
+  //     socket.emit("helloWorld", data.name)
+  //     db.close();
+  //   });
+  // });  Just Test
+ 
+
 });
 
 http.listen(3000, function() {
---
+++ server/schema.js
@@ -0,0 +1,13 @@
+// var mongoose = require("mongoose");
+
+// mongoose.connect("");
+
+// var Schema = mongoose.Schema;
+
+// var  dummySchema = new Schema({
+//   name: String
+// });
+
+// var Duumy = mongoose.model("duumy", dummySchema);
+
+// module.exports = Duumy;
