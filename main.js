57764bef2d2699cbd1b56fad918d0577916281cf
Kunanan <kunanan.tas@mail.kmutt.ac.th>
2018-04-19 22:05:20 +0700

    [Edit] remove system tray

--- main.js
+++ main.js
@@ -10,9 +10,7 @@ const url = require("url");
 const path = require("path");
 const { autoUpdater } = require("electron-updater");
 
-const iconPath = path.join(__dirname, "icon.png");
 let main;
-let appIcon;
 
 app.setAppUserModelId("treatlab.com");
 app.on("ready", () => {
@@ -28,25 +26,7 @@ app.on("ready", () => {
   );
   autoUpdater.checkForUpdates();
 
-  main.on("minimize", function(event) {
-    event.preventDefault();
-    main.hide();
-  });
-
-  main.on("close", function(event) {
-    if (!app.isQuiting) {
-      event.preventDefault();
-      main.hide();
-    }
-    return false;
-  });
-
-  const contextMenu = Menu.buildFromTemplate(contextMenuTemplate);
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
-
-  appIcon = new Tray(iconPath);
-  appIcon.setToolTip("Treatlap is running.");
-  appIcon.setContextMenu(contextMenu);
 
   Menu.setApplicationMenu(mainMenu);
 
@@ -78,21 +58,6 @@ const menuTemplate = [
   }
 ];
 
-const contextMenuTemplate = [
-  {
-    label: "Show App",
-    click: function() {
-      main.show();
-    }
-  },
-  {
-    label: "Quit",
-    click: function() {
-      app.isQuiting = true;
-      app.quit();
-    }
-  }
-];
 
 if (process.platform === "darwin") {
   menuTemplate.unshift({});
