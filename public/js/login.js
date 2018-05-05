6fbd12729354ae7ea2f9d7e89313341bb33b8f8d
Kunanan <kunanan.tas@mail.kmutt.ac.th>
2018-05-05 21:28:32 +0700

    [Edit] new signup func

--- public/js/login.js
+++ public/js/login.js
@@ -1,35 +1,56 @@
-let signUpBtn = document.getElementById("signUpBtn");
-let signInBtn = document.getElementById("signInBtn");
-
-signUpBtn.addEventListener("click", function() {
+function signup() {
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
   firebase
     .auth()
     .createUserWithEmailAndPassword(email, password)
     .then(function() {
-      alert("User create.");
+      let gender = document.getElementById('gender').value;
+      let age = document.getElementById('age').value;
+      let height = document.getElementById('height').value;
+      let weight = document.getElementById('weight').value;
+
+      let user = firebase.auth().currentUser;
+      let userRef = firebase.database().ref(`users/${user.uid}`);
+      userRef.set({
+        gender,
+        age,
+        height,
+        weight
+      }).then(() => {
+        alert('Registration completed')
+        window.location.href = "login.html"
+      })
     })
     .catch(function(err) {
       if (err != null) {
-        console.log(err.message);
+        alert(err.message);
         return;
       }
     });
-});
-signInBtn.addEventListener("click", function() {
+}
+
+function login(){
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
   firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
     .then(function() {
-      document.location.href = "main.html";
+      window.location.href = "main.html";
     })
     .catch(function(err) {
       if (err != null) {
         console.log(err.message);
         return;
       }
-    });
-});
+    });   
+}
+
+function backToLogin() {
+  document.location.href = "login.html"
+}
+
+function backtoSignup() {
+  document.location.href = 'signup.html'
+}
